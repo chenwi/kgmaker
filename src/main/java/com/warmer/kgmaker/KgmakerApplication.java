ec6e802cc781403fb3b06b3ba71b3366bd148baa
chenwi <1587504387@qq.com>
2019-05-29 16:35:43 +0800

    new

--- src/main/java/com/warmer/kgmaker/KgmakerApplication.java
+++ src/main/java/com/warmer/kgmaker/KgmakerApplication.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan
 public class KgmakerApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(KgmakerApplication.class, args);
 	}
